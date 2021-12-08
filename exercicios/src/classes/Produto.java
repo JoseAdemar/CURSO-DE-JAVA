6270a2c3f7c0a7683022d5ffa5cce497744d6fed
jose_ademar <joseademar.junior@shift.com.br>
2021-12-07 21:42:31 -0300

    Estudando metodos

--- exercicios/src/classes/Produto.java
+++ exercicios/src/classes/Produto.java
@@ -6,6 +6,7 @@ public class Produto {
 	Double preco;
 	Double desconto;
 
+	// Construtor com argumentos
 	public Produto(String nome, Double preco, Double desconto) {
 
 		this.nome = nome;
@@ -13,7 +14,22 @@ public class Produto {
 		this.desconto = desconto;
 	}
 	
+	// Construtor sem argumentos
 	public Produto() {
 		
 	}
+	
+	// Metodo
+	public Double precoComDesconto() {
+		
+		 return this.preco * (1 - this.desconto);
+	}
+	
+	// Metodo
+	public Double precoComDesconto(double descontoDoGerente) {
+		
+		 return this.preco * (1 - this.desconto - descontoDoGerente);
+	}
+	
+	
 }
--- exercicios/src/classes/ProdutoTeste.java
+++ exercicios/src/classes/ProdutoTeste.java
@@ -11,16 +11,18 @@ public class ProdutoTeste {
 
 		Produto p2 = new Produto();
 		p2.nome = "Caneta";
-		p2.preco = 12.53;
+		p2.preco = 12.56;
 		p2.desconto = 0.29;
 		
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
 		
-		Double precoFinalP1 = p1.preco * (1 - p1.desconto);
+		Double precoFinalP1 = p1.precoComDesconto();
+		Double precoFinalP01 = p1.precoComDesconto(0.1);
 		System.out.printf("Preço final notebook: R$%.2f\n",precoFinalP1);
+		System.out.printf("Preço final notebook desconto do gerente: R$%.2f\n",precoFinalP01);
 		
-		Double precoFinalP2 = p2.preco * (1 - p2.desconto);
+		Double precoFinalP2 = p2.precoComDesconto();
 		System.out.printf("Preço final caneta: R$%.2f\n", precoFinalP2);
 		
 		Double mediaCarrinho = (precoFinalP1 + precoFinalP2) / 2;
