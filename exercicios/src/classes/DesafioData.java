7557c494ca2b0435e974f838351008948833bf52
jose_ademar <joseademar.junior@shift.com.br>
2021-12-09 21:28:48 -0300

    Resolvido o desafio do metodo imprimir data

--- exercicios/src/classes/DesafioData.java
+++ exercicios/src/classes/DesafioData.java
@@ -5,4 +5,17 @@ public class DesafioData {
 	Integer dia;
 	Integer mes;
 	Integer ano;
+
+	// metodo 01
+	public String obterDataFormatada01(int dia, int mes, int ano) {
+
+		return "Hoje é: " + dia + "/" + mes + "/" + ano;
+
+	}
+
+	// metodo 02
+	public String obterDataFormatada02() {
+
+		return String.format("Hoje é: %d/%d/%d", dia, mes, ano);
+	}
 }
--- exercicios/src/classes/DesafioDataTeste.java
+++ exercicios/src/classes/DesafioDataTeste.java
@@ -3,19 +3,29 @@ package classes;
 public class DesafioDataTeste {
 
 	public static void main(String[] args) {
-		
+
 		DesafioData data1 = new DesafioData();
 		data1.dia = 25;
 		data1.mes = 11;
 		data1.ano = 2021;
-		
+
 		DesafioData data2 = new DesafioData();
 		data2.dia = 01;
 		data2.mes = 01;
 		data2.ano = 2022;
-		
-		System.out.printf("Hoje é: %d/%d/%d\n", data1.dia,data1.mes,data1.ano);
-		System.out.printf("Hoje é: %d/%d/%d", data2.dia,data2.mes,data2.ano);
+
+		// chamada do metodo 01
+		System.out.println(data1.obterDataFormatada01(data1.dia, data1.mes, data1.ano));
+		System.out.println(data2.obterDataFormatada01(data2.dia, data2.mes, data2.ano));
+		System.out.println("-----------------------------------------");
+
+		// chamada do metodo 02
+		System.out.println(data1.obterDataFormatada02());
+		System.out.println(data2.obterDataFormatada02());
+		System.out.println("-----------------------------------------");
+
+		System.out.printf("Hoje é: %d/%d/%d\n", data1.dia, data1.mes, data1.ano);
+		System.out.printf("Hoje é: %d/%d/%d", data2.dia, data2.mes, data2.ano);
 
 	}
 
