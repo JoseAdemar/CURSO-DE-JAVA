68cc76d7c84085a6415b5d8313e0f35e7ab97bfb
jose_ademar <joseademarti@gmail.com>
2022-01-25 21:56:21 -0300

    This e variaveis locais e valores padroes

---
+++ exercicios/src/classes/ValorNulo.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class ValorNulo {
+	public static void main(String[] args) {
+
+		String s1 = "";
+		System.out.println(s1 + "!!!!");
+		
+		//String s2 = null;
+		//System.out.println(s2.concat(s1)); // Nesse exemplo vai dar um nullPointException
+	}
+}
